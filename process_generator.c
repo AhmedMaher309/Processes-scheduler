3d8e620c8f54b9652c98505273067a1a2769ee7b
eng.omar.al.sharif <eng.omar.al.sharif@gmail.com>
2022-12-06 14:40:34 +0200

    Reading processes

--- process_generator.c
+++ process_generator.c
@@ -1,12 +1,58 @@
 #include "headers.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct {
+    int id;
+    int arrivalTime;
+    int runTime;
+    int priority;
+
+} Process;
 
 void clearResources(int);
+
+int getNumberOfLines(char* fileName)
+{
+    int count=0;
+    FILE* filePtr=fopen(fileName, "r");
+    char temp;
+    temp= getc(filePtr);
+    while (temp!=EOF)
+        {
+            if(temp=='\n')
+                count++;
+            temp=getc(filePtr);
+        }
+    fclose(filePtr);
+    return count;
+}
+
+void readInputFiles(char* fileName, Process* processArray, int processCount)
+{
+    FILE* filePtr=fopen(fileName, "r");
+    //Skip first line    
+    fscanf(filePtr, "%*[^\n]\n");
+    for (int i=0; i<processCount; i++)
+    {
+        fscanf(filePtr, "%d  %d  %d  %d", &(processArray[i].id), &(processArray[i].arrivalTime),
+        &(processArray[i].runTime), &(processArray[i].priority));
+    }
+    fclose(filePtr);
+}
 
 int main(int argc, char *argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
+
+    char* fileName= argv[1];
+    int processCount=getNumberOfLines(fileName);
+    Process processArray[processCount];
+    readInputFiles(fileName, processArray, processCount);
+    
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock.
